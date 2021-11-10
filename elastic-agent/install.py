660c2c57db922e787b18246b8ae9b8b004eab648
Jonathan Echavarria <jrec93@gmail.com>
2021-11-10 17:34:42 +0000

    updated envvars and elastic-agent runner

--- elastic-agent/install.py
+++ elastic-agent/install.py
@@ -19,13 +19,18 @@ if __name__ == "__main__":
     # agent.install(version=os.environ.get('ELK_VERSION'), preflight_check=preflight)
     verify_ssl = False if os.environ.get('FLEET_ENROLL_INSECURE') else True
     preflight = True if os.environ.get('PREFLIGHT_CHECK') else False
-    os.system(f"wget https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-{os.environ.get('ELK_VERSION')}-linux-x86_64.tar.gz")
-    os.system(f"tar zxvf elastic-agent-{os.environ.get('ELK_VERSION')}-linux-x86_64.tar.gz")
+    Get 
+    os.system(f"wget https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-{os.environ.get('ELK_VERSION')}-linux-x86_64.tar.gz 1>/proc/1/fd/1 2>/proc/1/fd/2")
+    os.system(f"tar zxvf elastic-agent-{os.environ.get('ELK_VERSION')}-linux-x86_64.tar.gz 1>/proc/1/fd/1 2>/proc/1/fd/2")
     os.system(f"cd elastic-agent-{os.environ.get('ELK_VERSION')}-linux-x86_64")
     # Installs the elastic-agent, this will fail if you already have a fleet server set up
-    os.system(f"/elastic-agent-{os.environ.get('ELK_VERSION')}-linux-x86_64/./elastic-agent install -f --fleet-server-es={os.environ.get('ELASTICSEARCH_HOSTS')} --fleet-server-service-token={os.environ.get('FLEET_SERVER_SERVICE_TOKEN')} --fleet-server-policy={os.environ.get('FLEET_SERVER_POLICY')} --fleet-server-es-ca=/ca.crt --fleet-server-cert=/fleet-server.crt --fleet-server-cert-key=/fleet-server.key --url={os.environ.get('FLEET_URL')} --certificate-authorities=/ca.crt --insecure")
+    os.system(f"/elastic-agent-{os.environ.get('ELK_VERSION')}-linux-x86_64/./elastic-agent install -f --fleet-server-es={os.environ.get('ELASTICSEARCH_HOSTS')} --fleet-server-service-token={os.environ.get('FLEET_SERVER_SERVICE_TOKEN')} --fleet-server-policy={os.environ.get('FLEET_SERVER_POLICY')} --fleet-server-es-ca=/ca.crt --fleet-server-cert=/fleet-server.crt --fleet-server-cert-key=/fleet-server.key --url={os.environ.get('FLEET_URL')} --certificate-authorities=/ca.crt --insecure 1>/proc/1/fd/1 2>/proc/1/fd/2")
     # Ensures elastic-agent is enrolled, AND pipes the logs so that it can be read by docker
     os.system(f"/usr/bin/elastic-agent enroll -f --fleet-server-es={os.environ.get('ELASTICSEARCH_HOSTS')} --fleet-server-service-token={os.environ.get('FLEET_SERVER_SERVICE_TOKEN')} --fleet-server-policy={os.environ.get('FLEET_SERVER_POLICY')} --fleet-server-es-ca=/ca.crt --fleet-server-cert=/fleet-server.crt --fleet-server-cert-key=/fleet-server.key --url={os.environ.get('FLEET_URL')} --certificate-authorities=/ca.crt --insecure 1>/proc/1/fd/1 2>/proc/1/fd/2 &")
+    # Wait 30 seconds just in case the enroll fails to execute the elastic-agent
+    time.sleep(30)
+    # Finally, run elstic agent
+    os.system(f"/usr/bin/elastic-agent run 1>/proc/1/fd/1 2>/proc/1/fd/2 &")
     while True:
         print('Elastic Agent is running .....', flush=True)
         time.sleep(30)
